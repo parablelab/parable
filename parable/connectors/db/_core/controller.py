53165a27a4a658bb3c4286febafea27ea6f43ba9
ajfloersch <ajfloersch@gmail.com>
2023-08-07 06:22:40 -0400

    PARA-14 Outlined intial set of standard operations.

--- parable/connectors/db/_core/controller.py
+++ parable/connectors/db/_core/controller.py
@@ -1,3 +1,25 @@
 
 class DBController:
-    pass
+
+
+    def create_table(self):
+        pass
+
+
+    def update_table(self):
+        pass
+
+
+    def merge_table(self):
+        pass
+
+
+    def truncate_table(self):
+        pass
+
+
+    def drop_table(self):
+        pass
+
+
+    
